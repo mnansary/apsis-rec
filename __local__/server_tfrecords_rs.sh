f1d8e15e6ef7fce705e283f845dfc969a80e1d16
silentassasin0111 <silentassasin0111@gmail.com>
2021-08-13 21:56:07 +0600

    Noise and BoxNoise

--- __local__/server_tfrecords_rs.sh
+++ __local__/server_tfrecords_rs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bw/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bw.test ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bs/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bs.test ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bh/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bh.test ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bw/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bw.train ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bs/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bs.train ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bh/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bh.train ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bangla.graphemes/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.bn.words ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bangla.numbers/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.bn.numbers ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/english.graphemes/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.en.words ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/english.numbers/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.en.numbers ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bangla.fontfaced/ /home/apsisdev/ansary/DATASETS/Recognition/ ff.bn ROBUSTSCANNER && \
-python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/english.fontfaced/ /home/apsisdev/ansary/DATASETS/Recognition/ ff.en ROBUSTSCANNER
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bw/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bw.test ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bs/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bs.test ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bh/test/ /home/apsisdev/ansary/DATASETS/Recognition/ bh.test ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bw/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bw.train ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bs/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bs.train ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bh/train/ /home/apsisdev/ansary/DATASETS/Recognition/ bh.train ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bangla.graphemes/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.bn.words ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/bangla.numbers/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.bn.numbers ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/english.graphemes/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.en.words ROBUSTSCANNER && \
+# python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/processed/english.numbers/ /home/apsisdev/ansary/DATASETS/Recognition/ synth.en.numbers ROBUSTSCANNER && \
+python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/fix/bangla.fontfaced/ /home/apsisdev/ansary/DATASETS/Recognition/ ff.bn ROBUSTSCANNER && \
+python /home/apsisdev/ansary/CODES/syntheticWords/tools/record.py  /home/apsisdev/ansary/DATASETS/Recognition/fix/english.fontfaced/ /home/apsisdev/ansary/DATASETS/Recognition/ ff.en ROBUSTSCANNER
 echo succeeded
