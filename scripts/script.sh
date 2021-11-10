650de1aea5273362f3be89195605ce71e6f02eaa
rezwanh001 <rezwanh001@gmail.com>
2021-11-10 12:34:48 +0600

    added english handwritten

--- scripts/script.sh
+++ scripts/script.sh
@@ -8,6 +8,7 @@ bw_ref="/home/apsisdev/ansary/DATASETS/RAW/bangla_writing/converted/converted/"
 bh_ref="/home/apsisdev/ansary/DATASETS/RAW/BN-HTR/"
 bs_ref="/home/apsisdev/ansary/DATASETS/RAW/BanglaC/README.txt"
 iit_path="/home/apsisdev/Rezwan/cvit_iiit-indic/"
+eng_hw_path="/home/apsisdev/ansary/DATASETS/RAW/eng_page/data/"
 
 save_path=$base_path
 src_dir="${base_path}source/"
@@ -22,23 +23,27 @@ bn_pr_ds="${ds_path}bangla_printed/"
 bn_hr_ds="${ds_path}bangla_handwritten/"
 iit_bn_ds="${ds_path}iit.bn/"
 
+en_hr_ds="${ds_path}en_hw/"
+
 #-----------------------------------bangla-----------------------------------------------
 #-----------------------------------natrual---------------------------------------------
-python datasets/bangla_writing.py $bw_ref $ds_path
-python datagen.py $bw_ds 
-python datasets/boise_state.py $bs_ref $ds_path
-python datagen.py $bs_ds 
-python datasets/bn_htr.py $bh_ref $ds_path
-python datagen.py $bh_ds 
-python datasets/iit_indic.py $iit_bn_ref $ds_path
-python datagen.py $iit_bn_ds 
+# python datasets/bangla_writing.py $bw_ref $ds_path
+# python datagen.py $bw_ds 
+# python datasets/boise_state.py $bs_ref $ds_path
+# python datagen.py $bs_ds 
+# python datasets/bn_htr.py $bh_ref $ds_path
+# python datagen.py $bh_ds 
+# python datasets/iit_indic.py $iit_bn_ref $ds_path
+# python datagen.py $iit_bn_ds 
 #-----------------------------------natrual---------------------------------------------
 
 #-----------------------------------synthetic------------------------------------------
-python datagen_synth.py $src_dir "bangla" "printed" $ds_path --num_samples 1000000
-python datagen_synth.py $src_dir "bangla" "handwritten" $ds_path --num_samples 100000
+# python datagen_synth.py $src_dir "bangla" "printed" $ds_path --num_samples 1000000
+# python datagen_synth.py $src_dir "bangla" "handwritten" $ds_path --num_samples 100000
 #-----------------------------------synthetic------------------------------------------
 #-----------------------------------bangla-----------------------------------------------
 
+#-----------------------------------english-----------------------------------------------
+python datasets/eng_hw.py $eng_hw_path $ds_path
 
 echo succeeded
